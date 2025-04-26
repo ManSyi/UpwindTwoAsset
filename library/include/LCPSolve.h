9c957beedc5b9184d506a494118d1e0627ef2489
m15216713169_1 <m15216713169_1@163.com>
2025-04-26 19:20:48 +0800

    add lcp solver

---
+++ library/include/LCPSolve.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Eigen/Dense>
+
+namespace LCPSolve
+{
+    // Linear complementarity problem datatype.
+    struct LCP {
+        int exitCond;
+        Eigen::VectorXd w{};
+        Eigen::VectorXd z{};
+        Eigen::MatrixXd M{};
+        Eigen::VectorXd q{};
+    };
+
+    // LCP Solver.
+    LCP LCPSolve(Eigen::MatrixXd M, Eigen::VectorXd q);
+
+
+}
